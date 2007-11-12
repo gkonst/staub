2a04db8b6771f4d853573e9b1e34e752293659ed
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-12 21:22:03 +0000

    draft user module introdused

---
+++ staub/staubEJB/src/ru/spbspu/staub/entity/User.java
@@ -0,0 +1,35 @@
+package ru.spbspu.staub.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    private String username;
+    private String password;
+
+    @Id
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+   @Override
+   public String toString() {
+      return "User(" + username + ")";
+   }
+}
