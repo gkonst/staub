c4e0679f4318372b7a31f96a1ebfc7c24dee1800
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-14 14:22:17 +0000

    generic detail and list webbeans implementations added

---
+++ trunk/staub/staubWEB/src/ru/spbspu/staub/bean/GenericListBean.java
@@ -0,0 +1,34 @@
+package ru.spbspu.staub.bean;
+
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.faces.DataModels;
+
+import javax.faces.model.DataModel;
+import java.util.List;
+
+/**
+ * TODO add descritpion
+ *
+ * @author Konstantin Grigoriev
+ */
+public abstract class GenericListBean<T> extends GenericBean {
+
+    private DataModel dataModel;
+
+    protected abstract List<T> getResultList();
+
+    @Create
+    public void initList() {
+        logger.debug("--> Constructing list bean...");
+        dataModel = DataModels.instance().getDataModel(getResultList());
+        logger.debug("<-- Constructing list bean...Ok");
+    }
+
+    public DataModel getDataModel() {
+        return dataModel;
+    }
+
+    public void setDataModel(DataModel dataModel) {
+        this.dataModel = dataModel;
+    }
+}
