21252287ad0f4c04dd58f0a9b0e8a8835bde2ba2
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-14 14:22:58 +0000

    test data added

--- trunk/staub/resources/import.sql
+++ trunk/staub/resources/import.sql
@@ -1,2 +1,8 @@
 insert into users (username, password) values ('test', 'test')
+insert into users (username, password) values ('user', 'user')
+insert into test (id, name) values(1, 'test1')
+insert into test (id, name) values(2, 'test2')
+insert into test (id, name) values(3, 'test3')
+insert into test (id, name) values(4, 'test4')
+insert into test (id, name) values(5, 'test5')
 
