edd24a1f80c3fa8326eb2699a76c8b4a8a8e0c7c
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-14 13:12:42 +0000

    generci web bean introduced

--- staub/staubWEB/src/ru/spbspu/staub/bean/LoginBean.java
+++ staub/staubWEB/src/ru/spbspu/staub/bean/LoginBean.java
@@ -1,11 +1,9 @@
 package ru.spbspu.staub.bean;
 
-import org.jboss.seam.ScopeType;
+import static org.jboss.seam.ScopeType.EVENT;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Out;
-import org.jboss.seam.log.Log;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.security.Identity;
 import ru.spbspu.staub.dao.UserDAO;
 import ru.spbspu.staub.entity.User;
@@ -16,19 +14,15 @@ import ru.spbspu.staub.entity.User;
  * @author Konstantin Grigoriev
  */
 @Name("loginBean")
-public class LoginBean {
-    @Logger
-    private Log logger;
-
-    @Out(required = false, scope = ScopeType.SESSION)
-    private User user;
+@Scope(EVENT)
+public class LoginBean extends GenericBean {
 
     @In
     private UserDAO userDAO;
 
     public boolean authenticate() {
         logger.debug("--> Authentinicating user(username=#{identity.username}), password=#{identity.password}");
-        user = userDAO.findUserByUserNameAndPassword(Identity.instance().getUsername(), Identity.instance().getPassword());
+        User user = userDAO.findUserByUserNameAndPassword(Identity.instance().getUsername(), Identity.instance().getPassword());
         logger.debug("<-- Authentinicating ok(result=" + (user != null) + ").");
         return user != null;
     }
