02a862382ba9e8c026d6992e7271cfaa98299895
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-14 10:49:01 +0000

    core genericDAO functionality implemented

--- trunk/staub/staubEJB/src/ru/spbspu/staub/dao/GenericDAO.java
+++ trunk/staub/staubEJB/src/ru/spbspu/staub/dao/GenericDAO.java
@@ -1,6 +1,7 @@
 package ru.spbspu.staub.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Base interface for dao interfaces,
@@ -8,5 +9,33 @@ import java.io.Serializable;
  * @author Konstantin Grigoriev
  */
 public interface GenericDAO<T, ID extends Serializable> {
-    T findById(ID id);
+    /**
+     * Fetches entity by specific id.
+     * @param id id to fetch
+     * @param lock need to get lock or not
+     * @return fetched entity
+     */
+    T findById(ID id, boolean lock);
+
+    /**
+     * Fetches all entities.
+     * May cause <code>java.lang.OutOfMemoryError</code> in case
+     * big amount of entities.
+     * @return parametrized list of entities
+     */
+    List<T> findAll();
+
+    /**
+     * Makes entity persistent(i.e. inserts to table).
+     * Uses <code>EntityManager.merge(Object entity)</code> method.
+     * @param entity entity to persist
+     * @return persistent entity(with id field filled)
+     */
+    T makePersistent(T entity);
+
+    /**
+     * Makes entity transient(i.e. deletes from table).
+     * @param entity entity to remove
+     */
+    void makeTransient(T entity);
 }
--- trunk/staub/staubEJB/src/ru/spbspu/staub/dao/GenericDAOBean.java
+++ trunk/staub/staubEJB/src/ru/spbspu/staub/dao/GenericDAOBean.java
@@ -1,15 +1,88 @@
 package ru.spbspu.staub.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
 /**
- * TODO add descritpion
- *
+ * Implements the generic CRUD data access operations using Java Persistence APIs.
+ * Uses traditional 1:1 appraoch for Entity:DAO design.
  * @author Konstantin Grigoriev
  */
 public class GenericDAOBean<T, ID extends Serializable> implements GenericDAO<T, ID>{
-    public T findById(ID id) {
-        // TODO implement
-        return null;
+
+    private Class<T> entityClass;
+
+    private EntityManager em;
+
+    @SuppressWarnings("unchecked")
+    public GenericDAOBean() {
+        this.entityClass = (Class<T>) ((ParameterizedType) getClass()
+                                .getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public T findById(ID id, boolean lock) {
+        T entity;
+        if (lock) {
+            entity = getEntityManager().find(getEntityClass(), id);
+            em.lock(entity, javax.persistence.LockModeType.WRITE);
+        } else {
+            entity = getEntityManager().find(getEntityClass(), id);
+        }
+        return entity;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public List<T> findAll() {
+        return getEntityManager().createQuery("from " + getEntityClass().getName() ).getResultList();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public T makePersistent(T entity) {
+        return getEntityManager().merge(entity);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void makeTransient(T entity) {
+        getEntityManager().remove(entity);
+    }
+
+    public void flush() {
+        getEntityManager().flush();
+    }
+
+    public void clear() {
+        getEntityManager().clear();
+    }
+
+    @PersistenceContext
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+
+    protected EntityManager getEntityManager() {
+        if (em == null)
+            throw new IllegalStateException("EntityManager has not been set on DAO before usage");
+        return em;
+    }
+
+    public Class<T> getEntityClass() {
+        return entityClass;
+    }
+
+    public void setEntityClass(Class<T> entityClass) {
+        this.entityClass = entityClass;
     }
 }
