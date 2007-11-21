9611a18633ea2f832a8cb2d0ee95f4a42c66f22b
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-21 13:20:22 +0000

    javadocs comments fixed

--- trunk/staub/staubEJB/src/ru/spbspu/staub/dao/UserDAO.java
+++ trunk/staub/staubEJB/src/ru/spbspu/staub/dao/UserDAO.java
@@ -5,11 +5,17 @@ import ru.spbspu.staub.entity.User;
 import javax.ejb.Local;
 
 /**
- * TODO add descritpion
+ * Local Interface for <code>UserDAOBean</code> Stateless EJB.
  *
  * @author Konstantin Grigoriev
  */
 @Local
 public interface UserDAO extends GenericDAO<User, String> {
+    /**
+     * Fetches specific user by username and password.
+     * @param username specific user name
+     * @param password specific user password
+     * @return fetched user
+     */
     User findUserByUserNameAndPassword(String username, String password);
 }
