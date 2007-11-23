16361d13d2863c9f4ba7e42827531eaeef0899ad
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2007-11-23 15:54:25 +0000

    iterator replaced by index integer, previous funxtions added

--- trunk/staub/staubWEB/src/ru/spbspu/staub/bean/QuestionDetailBean.java
+++ trunk/staub/staubWEB/src/ru/spbspu/staub/bean/QuestionDetailBean.java
@@ -8,8 +8,7 @@ import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.web.RequestParameter;
 import ru.spbspu.staub.dao.TestDAO;
 import ru.spbspu.staub.entity.Question;
-
-import java.util.Iterator;
+import ru.spbspu.staub.entity.Test;
 
 
 /**
@@ -27,20 +26,32 @@ public class QuestionDetailBean extends GenericBean {
     @In
     private TestDAO testDAO;
 
+    private Test test;
     private Question currentQuestion;
-
-    private Iterator<Question> questionIterator;
+    private int questionIndex = 0;
 
     @Create
     public void startTest() {
         logger.debug(">>> Starting test(#0)...", modelId);
-        questionIterator = testDAO.findById(modelId, true).getQuestions().iterator();
-        currentQuestion = questionIterator.next();
+        test = testDAO.findById(modelId, true);
+        currentQuestion = test.getQuestions().get(questionIndex);
         logger.debug("<<< Starting test...Ok");
     }
 
+     public void previousQuestion() {
+        currentQuestion = test.getQuestions().get(--questionIndex);
+    }
+
     public void nextQuestion() {
-        currentQuestion = questionIterator.next();
+        currentQuestion = test.getQuestions().get(++questionIndex);
+    }
+
+    public boolean isHasPreviousQuestion() {
+        return questionIndex > 0;
+    }
+
+    public boolean isHasNextQuestion() {
+        return (questionIndex + 1) < test.getQuestions().size();
     }
 
     public Question getCurrentQuestion() {
@@ -51,7 +62,11 @@ public class QuestionDetailBean extends GenericBean {
         this.currentQuestion = currentQuestion;
     }
 
-    public boolean isHasNextQuestion() {
-        return questionIterator.hasNext();    
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
     }
 }
