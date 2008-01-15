77414d749514ffb096ef1b8a9ab0ca76b73dbf45
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-15 18:05:21 +0000

    templates for substitution added

--- branches/staub-mvn/db/src/main/resources/create_schema.sql
+++ branches/staub-mvn/db/src/main/resources/create_schema.sql
@@ -1,2 +1,2 @@
-CREATE USER staub PASSWORD 'staub';
-CREATE SCHEMA AUTHORIZATION staub;
+CREATE USER ${db.staub.username} PASSWORD '${db.staub.password}';
+CREATE SCHEMA AUTHORIZATION ${db.staub.username};
