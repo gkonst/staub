1a54caf72c75342c976ebb1536cb9f7912b59415
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-18 16:15:23 +0000

    XML parsing fields and methods added - VERY DRAFT VERSION

--- branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/QuestionDetailBean.java
+++ branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/QuestionDetailBean.java
@@ -9,6 +9,18 @@ import org.jboss.seam.annotations.web.RequestParameter;
 import ru.spbspu.staub.dao.TestDAO;
 import ru.spbspu.staub.entity.Question;
 import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.model.question.AnswerType;
+import ru.spbspu.staub.model.question.QuestionType;
+
+import javax.faces.model.SelectItem;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -28,22 +40,73 @@ public class QuestionDetailBean extends GenericBean {
 
     private Test test;
     private Question currentQuestion;
+    private QuestionType questionDefinition;
     private int questionIndex = 0;
+
+    private Unmarshaller unmarshaller = null;
+
+    private String[] answerMultipleChoice = null;
+    private List<SelectItem> answerChoiceItems = null;
 
     @Create
     public void startTest() {
         logger.debug(">>> Starting test(#0)...", modelId);
         test = testDAO.findById(modelId, true);
         currentQuestion = test.getQuestions().get(questionIndex);
+        questionDefinition = parseQuestionDefinationXML(currentQuestion.getDefinition());
         logger.debug("<<< Starting test...Ok");
     }
 
-     public void previousQuestion() {
+    public void previousQuestion() {
         currentQuestion = test.getQuestions().get(--questionIndex);
+        questionDefinition = parseQuestionDefinationXML(currentQuestion.getDefinition());
     }
 
     public void nextQuestion() {
+        saveAnswer();
         currentQuestion = test.getQuestions().get(++questionIndex);
+        questionDefinition = parseQuestionDefinationXML(currentQuestion.getDefinition());
+    }
+
+    private QuestionType parseQuestionDefinationXML(String questionDefinationXML) {
+        try {
+            logger.debug(" Parsing question defination XML...");
+            logger.debug("  XML : " + questionDefinationXML);
+            if (unmarshaller == null) {
+                JAXBContext jaxbContext = JAXBContext.newInstance("ru.spbspu.staub.model.question");
+                unmarshaller = jaxbContext.createUnmarshaller();
+            }
+            JAXBElement jaxbElement = (JAXBElement) unmarshaller.unmarshal(new StreamSource(new StringReader(questionDefinationXML)));
+            QuestionType result = (QuestionType) jaxbElement.getValue();
+            logger.debug("  description : " + result.getDescription());
+            logger.debug("  multipleChoice : " + result.getMultipleChoice());
+            logger.debug("  singleChoice : " + result.getSingleChoice());
+            if(result.getMultipleChoice() != null) {
+                answerChoiceItems = new ArrayList<SelectItem>();
+                for(AnswerType choice : result.getMultipleChoice().getAnswer()) {
+                    logger.debug("    choice answer(correct=" + choice.getCorrect() + ", value=" + choice.getValue() + ")");
+                    answerChoiceItems.add(new SelectItem(String.valueOf(choice.getCorrect()), choice.getValue()));
+                }
+                answerMultipleChoice = new String[answerChoiceItems.size()];
+            } else if(result.getSingleChoice() != null) {
+                answerChoiceItems = new ArrayList<SelectItem>();
+                for(AnswerType choice : result.getSingleChoice().getAnswer()) {
+                    answerChoiceItems.add(new SelectItem(choice.getCorrect(), choice.getValue()));
+                }
+            }
+
+            logger.debug(" Parsing question defination XML...Ok");
+            return result;
+        } catch (JAXBException e) {
+            logger.error("Error during parsing question defination XML(" + questionDefinationXML + ")", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveAnswer() {
+        logger.debug(">>> Saving answers...");
+
+        logger.debug("<<< Saving answers...");
     }
 
     public boolean isHasPreviousQuestion() {
@@ -52,6 +115,26 @@ public class QuestionDetailBean extends GenericBean {
 
     public boolean isHasNextQuestion() {
         return (questionIndex + 1) < test.getQuestions().size();
+    }
+
+    public boolean isMultipleChoiceAnswer() {
+        return questionDefinition.getMultipleChoice() != null;
+    }
+
+    public List<SelectItem> getAnswerChoiceItems() {
+        return answerChoiceItems;
+    }
+
+    public void setAnswerChoiceItems(List<SelectItem> answerChoiceItems) {
+        this.answerChoiceItems = answerChoiceItems;
+    }
+
+    public String[] getAnswerMultipleChoice() {
+        return answerMultipleChoice;
+    }
+
+    public void setAnswerMultipleChoice(String[] answerMultipleChoice) {
+        this.answerMultipleChoice = answerMultipleChoice;
     }
 
     public Question getCurrentQuestion() {
@@ -68,5 +151,13 @@ public class QuestionDetailBean extends GenericBean {
 
     public void setTest(Test test) {
         this.test = test;
+    }
+
+    public QuestionType getQuestionDefinition() {
+        return questionDefinition;
+    }
+
+    public void setQuestionDefinition(QuestionType questionDefinition) {
+        this.questionDefinition = questionDefinition;
     }
 }
