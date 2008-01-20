38a91aa5e40b59518a833258189799223115fbe5
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-20 21:13:17 +0000

    findIdsByTestId draft method added

--- ejb/src/main/java/ru/spbspu/staub/dao/QuestionDAOBean.java
+++ ejb/src/main/java/ru/spbspu/staub/dao/QuestionDAOBean.java
@@ -5,9 +5,10 @@ import org.jboss.seam.annotations.Name;
 import ru.spbspu.staub.entity.Question;
 
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
- * Stateless EJB DAO for manipulations with <code>Question</code> entity. 
+ * Stateless EJB DAO for manipulations with <code>Question</code> entity.
  *
  * @author Konstantin Grigoriev
  */
@@ -15,4 +16,7 @@ import javax.ejb.Stateless;
 @AutoCreate
 @Stateless
 public class QuestionDAOBean extends GenericDAOBean<Question, Integer> implements QuestionDAO {
+    public List<Integer> findIdsByTestId(Integer testId) {
+        return getEntityManager().createQuery("from " + Question.class.getName() + " q where q.test.id = :testId").setParameter("testId", testId).getResultList();
+    }
 }
