2227fef82499bebe0574bad8cea71770c96205b0
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-20 21:11:18 +0000

    wrappers for answer added

---
+++ web/src/main/java/ru/spbspu/staub/model/MultipleChoiceAnswerWrapper.java
@@ -0,0 +1,44 @@
+package ru.spbspu.staub.model;
+
+import ru.spbspu.staub.model.question.ChoiceType;
+import ru.spbspu.staub.model.question.AnswerType;
+
+import javax.faces.model.SelectItem;
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * TODO add descritpion
+ *
+ * @author Konstantin Grigoriev
+ */
+public class MultipleChoiceAnswerWrapper extends AnswerWrapper {
+    private ChoiceType answerDefinition;
+    private List<SelectItem> answerItems = null;
+    private List<String> userAnswer = null;
+
+    public MultipleChoiceAnswerWrapper(ChoiceType answerDefinition) {
+        this.answerDefinition = answerDefinition;
+        answerItems = new ArrayList<SelectItem>();
+        userAnswer = new ArrayList<String>();
+        for (AnswerType choice : answerDefinition.getAnswer()) {
+            answerItems.add(new SelectItem(choice.getValue(), choice.getValue()));
+        }
+    }
+
+    public List<SelectItem> getAnswerItems() {
+        return answerItems;
+    }
+
+    public void setAnswerItems(List<SelectItem> answerItems) {
+        this.answerItems = answerItems;
+    }
+
+    public List<String> getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(List<String> userAnswer) {
+        this.userAnswer = userAnswer;
+    }
+}
