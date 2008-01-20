67a32cc7b4dbdc41f424c2f3dfe2500e8bd41305
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-20 21:12:21 +0000

    wrapper for question added

---
+++ web/src/main/java/ru/spbspu/staub/model/QuestionWrapper.java
@@ -0,0 +1,90 @@
+package ru.spbspu.staub.model;
+
+import ru.spbspu.staub.model.question.QuestionType;
+import ru.spbspu.staub.entity.Question;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.stream.StreamSource;
+import java.io.StringReader;
+
+import org.jboss.seam.log.Log;
+import org.jboss.seam.log.Logging;
+import org.jboss.seam.annotations.Logger;
+
+/**
+ * Wrapper encapsulates
+ *
+ * @author Konstantin Grigoriev
+ */
+public class QuestionWrapper {
+
+    private Log logger = Logging.getLog(QuestionWrapper.class);
+
+    private String name;
+    private String description;
+    private QuestionType definition;
+    private int timeLimit;
+
+    public QuestionWrapper(Question question) {
+        wrap(question);
+    }
+
+    private void wrap(Question question) {
+        this.name = question.getName();
+        this.definition = parseDefinitionXML(question.getDefinition());
+        this.description = this.definition.getDescription();
+    }
+
+    private QuestionType parseDefinitionXML(String definitionXML) {
+        logger.debug(" Parsing question definition XML...");
+        try {
+            logger.debug("  XML : " + definitionXML);
+            JAXBContext jaxbContext = JAXBContext.newInstance("ru.spbspu.staub.model.question");
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            JAXBElement jaxbElement = (JAXBElement) unmarshaller.unmarshal(new StreamSource(new StringReader(definitionXML)));
+            QuestionType result = (QuestionType) jaxbElement.getValue();
+            logger.debug("  description : " + result.getDescription());
+            logger.debug("  multipleChoice : " + result.getMultipleChoice());
+            logger.debug("  singleChoice : " + result.getSingleChoice());
+            return result;
+        } catch (JAXBException e) {
+            logger.error("Error during parsing question defination XML(" + definitionXML + ")", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTimeLimit() {
+        return timeLimit;
+    }
+
+    public void setTimeLimit(int timeLimit) {
+        this.timeLimit = timeLimit;
+    }
+
+    public QuestionType getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(QuestionType definition) {
+        this.definition = definition;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
