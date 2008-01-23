49fc4f1110965cfbf41e076faf92b6c143e74826
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-23 16:40:25 +0000

    timer methods added

--- branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/QuestionDetailBean.java
+++ branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/QuestionDetailBean.java
@@ -1,6 +1,8 @@
 package ru.spbspu.staub.bean;
 
 import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Conversational;
 import org.jboss.seam.annotations.Create;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
@@ -10,6 +12,7 @@ import ru.spbspu.staub.dao.QuestionDAO;
 import ru.spbspu.staub.model.AnswerWrapper;
 import ru.spbspu.staub.model.QuestionWrapper;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -20,6 +23,7 @@ import java.util.List;
  */
 @Name("questionDetailBean")
 @Scope(ScopeType.CONVERSATION)
+@Conversational
 public class QuestionDetailBean extends GenericBean {
 
     @RequestParameter
@@ -31,33 +35,65 @@ public class QuestionDetailBean extends GenericBean {
     private List<Integer> questionIds;
     private QuestionWrapper currentQuestion;
     private int questionIndex = 0;
+    private int currentTime = -1;
+    private long startTime;
 
     private AnswerWrapper answer;
 
+    @Begin
     @Create
-    public void startTest() {
-        logger.debug(">>> Starting test(#0)...", modelId);
+    public String initTest() {
+        logger.debug(">>> Init test(#0)...", modelId);
         questionIds = questionDAO.findIdsByTestId(modelId);
         currentQuestion = new QuestionWrapper(questionDAO.findById(questionIds.get(questionIndex), false));
-        answer = AnswerWrapper.getAnswer(currentQuestion.getDefinition());
-        logger.debug("<<< Starting test...Ok");
+        answer = AnswerWrapper.getAnswer(currentQuestion.getDefinition());  
+        logger.debug(" currentTime : " + currentTime);
+        if(currentTime == -1) {
+            resetTimer();
+        } else {
+            refreshTimer();
+        }
+        logger.debug("<<< Init test...Ok");
+        return "questionDetail";
     }
 
-    public void previousQuestion() {
+    public String previousQuestion() {
+        logger.debug(">>> Previous question...");
         currentQuestion = new QuestionWrapper(questionDAO.findById(questionIds.get(--questionIndex), false));
         answer = AnswerWrapper.getAnswer(currentQuestion.getDefinition());
+        resetTimer();
+        logger.debug(">>> Previous question...Ok");
+        return null;
     }
 
-    public void nextQuestion() {
+    public String nextQuestion() {
+        logger.debug(">>> Next question...");
         saveAnswer();
         currentQuestion = new QuestionWrapper(questionDAO.findById(questionIds.get(++questionIndex), false));
         answer = AnswerWrapper.getAnswer(currentQuestion.getDefinition());
+        resetTimer();
+        logger.debug(">>> Next question...Ok");
+        return null;
+    }
+
+    private void resetTimer() {
+        currentTime = currentQuestion.getTimeLimit();
+        startTime = new Date().getTime();
     }
 
     private void saveAnswer() {
-        logger.debug(">>> Saving answers...");
+        logger.debug(" >>> Saving answers...");
 
-        logger.debug("<<< Saving answers...");
+        logger.debug(" <<< Saving answers...Ok");
+    }
+
+    public void refreshTimer() {
+        logger.debug(">>> Refreshing timer...");
+        long newTime = (new Date().getTime() - startTime)/1000;
+        logger.debug(" time elapsed : " + newTime);
+        logger.debug(" time remaining : " + (currentQuestion.getTimeLimit() - (int)newTime));
+        this.currentTime = currentQuestion.getTimeLimit() - (int)newTime;
+        logger.debug(">>> Refreshing timer...Ok");
     }
 
     public boolean isHasPreviousQuestion() {
@@ -82,5 +118,13 @@ public class QuestionDetailBean extends GenericBean {
 
     public void setAnswer(AnswerWrapper answer) {
         this.answer = answer;
+    }
+
+    public int getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(int currentTime) {
+        this.currentTime = currentTime;
     }
 }
