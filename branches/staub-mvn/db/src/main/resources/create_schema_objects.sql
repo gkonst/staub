cb50258cf4ec87ce67b170668b50e45e09cdef26
Alexander.Elagin <Alexander.Elagin@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-25 17:17:21 +0000

    Added two new entities: QuestionTrace and TestTrace. These entities hold information about user's test state.

--- branches/staub-mvn/db/src/main/resources/create_schema_objects.sql
+++ branches/staub-mvn/db/src/main/resources/create_schema_objects.sql
@@ -1,3 +1,4 @@
+-- Tables
 CREATE TABLE question (
     id integer NOT NULL,
     fk_test integer NOT NULL,
@@ -8,6 +9,17 @@ CREATE TABLE question (
     created_by character varying(64),
     modified date,
     modified_by character varying(64)
+);
+
+CREATE TABLE question_trace (
+    id integer NOT NULL,
+    fk_test_trace integer NOT NULL,
+    fk_question integer NOT NULL,
+    fk_user integer NOT NULL,
+    session_id character varying(64) NOT NULL,
+    started date NOT NULL,
+    finished date,
+    answer text NOT NULL
 );
 
 CREATE TABLE test (
@@ -26,14 +38,29 @@ CREATE TABLE test (
     CONSTRAINT chk_time_limit CHECK ((time_limit > 0))
 );
 
+CREATE TABLE test_trace (
+    id integer NOT NULL,
+    fk_test integer NOT NULL,
+    fk_user integer NOT NULL,
+    session_id character varying(64) NOT NULL,
+    started date NOT NULL,
+    finished date
+);
+
 CREATE TABLE "user" (
     id integer NOT NULL,
     username character varying(64) NOT NULL,
     password character varying(64) NOT NULL
 );
 
+-- Sequences
 CREATE SEQUENCE seq_question
-    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 2147483647
+    NO MINVALUE
+    CACHE 1;
+
+CREATE SEQUENCE seq_question_trace
     INCREMENT BY 1
     MAXVALUE 2147483647
     NO MINVALUE
@@ -45,25 +72,64 @@ CREATE SEQUENCE seq_test
     NO MINVALUE
     CACHE 1;
 
+CREATE SEQUENCE seq_test_trace
+    INCREMENT BY 1
+    MAXVALUE 2147483647
+    NO MINVALUE
+    CACHE 1;
+
 CREATE SEQUENCE seq_user
     INCREMENT BY 1
     MAXVALUE 2147483647
     NO MINVALUE
     CACHE 1;
 
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT ak_user UNIQUE (username);
-
+-- Constraints
 ALTER TABLE ONLY question
     ADD CONSTRAINT pk_question PRIMARY KEY (id);
+
+ALTER TABLE ONLY question_trace
+    ADD CONSTRAINT pk_question_trace PRIMARY KEY (id);
 
 ALTER TABLE ONLY test
     ADD CONSTRAINT pk_test PRIMARY KEY (id);
 
+ALTER TABLE ONLY test_trace
+    ADD CONSTRAINT pk_test_trace PRIMARY KEY (id);
+
 ALTER TABLE ONLY "user"
     ADD CONSTRAINT pk_user PRIMARY KEY (id);
 
-CREATE INDEX fki_test ON question USING btree (fk_test);
+ALTER TABLE ONLY "user"
+    ADD CONSTRAINT ak_user UNIQUE (username);
 
 ALTER TABLE ONLY question
     ADD CONSTRAINT fk_test FOREIGN KEY (fk_test) REFERENCES test(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY question_trace
+    ADD CONSTRAINT fk_question FOREIGN KEY (fk_question) REFERENCES question(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY question_trace
+    ADD CONSTRAINT fk_test_trace FOREIGN KEY (fk_test_trace) REFERENCES test_trace(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY question_trace
+    ADD CONSTRAINT fk_user FOREIGN KEY (fk_user) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY test_trace
+    ADD CONSTRAINT fk_test FOREIGN KEY (fk_test) REFERENCES test(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE ONLY test_trace
+    ADD CONSTRAINT fk_user FOREIGN KEY (fk_user) REFERENCES "user"(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- Indexes
+CREATE INDEX fki_question__test ON question USING btree (fk_test);
+
+CREATE INDEX fki_question_trace__question ON question_trace USING btree (fk_question);
+
+CREATE INDEX fki_question_trace__test_trace ON question_trace USING btree (fk_test_trace);
+
+CREATE INDEX fki_question_trace__user ON question_trace USING btree (fk_user);
+
+CREATE INDEX fki_test_trace__test ON test_trace USING btree (fk_test);
+
+CREATE INDEX fki_test_trace__user ON test_trace USING btree (fk_user);
---
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/entity/TestTrace.java
@@ -0,0 +1,119 @@
+package ru.spbspu.staub.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * The <code>TestTrace</code> class represents TestTrace entity.
+ *
+ * @author Alexander V. Elagin
+ */
+@Entity
+@Table(schema = "staub", name = "test_trace")
+public class TestTrace implements Serializable {
+    private static final long serialVersionUID = 410759301262862769L;
+
+    private Integer id;
+
+    @Id
+    @SequenceGenerator(name = "TestTraceIdGenerator", sequenceName = "seq_test_trace", allocationSize = 1)
+    @GeneratedValue(generator = "TestTraceIdGenerator", strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false, length = 10)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private String sessionId;
+
+    @Basic
+    @Column(name = "session_id", nullable = false, length = 64)
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    private Date started;
+
+    @Basic
+    @Column(name = "started", nullable = false)
+    public Date getStarted() {
+        return started;
+    }
+
+    public void setStarted(Date started) {
+        this.started = started;
+    }
+
+    private Date finished;
+
+    @Basic
+    @Column(name = "finished")
+    public Date getFinished() {
+        return finished;
+    }
+
+    public void setFinished(Date finished) {
+        this.finished = finished;
+    }
+
+    private Test test;
+
+    @OneToOne
+    @JoinColumn(name = "fk_test", referencedColumnName = "id", nullable = false)
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "fk_user", referencedColumnName = "id", nullable = false)
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if (this == otherObject) {
+            return true;
+        }
+        if (!(otherObject instanceof TestTrace)) {
+            return false;
+        }
+
+        TestTrace other = (TestTrace) otherObject;
+
+        return id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TestTrace");
+        sb.append("{id=").append(id);
+        sb.append('}');
+
+        return sb.toString();
+    }
+}
