ab501ae2d761aafce96128f6b32879b42f2864ba
Alexander.Elagin <Alexander.Elagin@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-01-31 15:52:37 +0000

    Fixed initial data script according to XSD.

--- db/src/main/resources/test_data.sql
+++ db/src/main/resources/test_data.sql
@@ -32,7 +32,7 @@ INSERT INTO question (id,
      VALUES (nextval('seq_question'), 
              currval('seq_test'),
              'Question #1',
-             '<?xml version="1.0" encoding="UTF-8"?><question xmlns="http://staub.spbspu.ru/Question"><description>How many fingers does the average homo sapiens have? (Choose one.)</description><single-choice><answer correct="true">5</answer><answer>10</answer><answer>15</answer><answer>20</answer></single-choice></question>');
+             '<?xml version="1.0" encoding="UTF-8"?><question xmlns="http://staub.spbspu.ru/Question"><description>How many fingers does the average homo sapiens have? (Choose one.)</description><single-choice><answer id="1" correct="true">5</answer><answer id="2">10</answer><answer id="3">15</answer><answer id="4">20</answer></single-choice></question>');
 
 INSERT INTO question (id,
                       fk_test,
@@ -41,4 +41,4 @@ INSERT INTO question (id,
      VALUES (nextval('seq_question'), 
              currval('seq_test'),
              'Question #2',
-             '<?xml version="1.0" encoding="UTF-8"?><ques:question xmlns:ques="http://staub.spbspu.ru/Question"><ques:description>How many days are in February? (Choose multiple.)</ques:description><ques:multiple-choice><ques:answer correct="true">28</ques:answer><ques:answer correct="true">29</ques:answer><ques:answer>30</ques:answer><ques:answer>31</ques:answer></ques:multiple-choice></ques:question>');
+             '<?xml version="1.0" encoding="UTF-8"?><ques:question xmlns:ques="http://staub.spbspu.ru/Question"><ques:description>How many days are in February? (Choose multiple.)</ques:description><ques:multiple-choice><ques:answer id="1" correct="true">28</ques:answer><ques:answer id="2" correct="true">29</ques:answer><ques:answer id="3">30</ques:answer><ques:answer id="4">31</ques:answer></ques:multiple-choice></ques:question>');
