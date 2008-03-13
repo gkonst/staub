39ee376548f551d696251a93c8dd81c9669a038f
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-13 16:30:09 +0000

    user outjection added

--- branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/LoginBean.java
+++ branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/LoginBean.java
@@ -4,7 +4,9 @@ import static org.jboss.seam.ScopeType.EVENT;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Out;
 import org.jboss.seam.security.Identity;
+import org.jboss.seam.ScopeType;
 import ru.spbspu.staub.entity.User;
 import ru.spbspu.staub.service.UserService;
 
@@ -20,9 +22,12 @@ public class LoginBean extends GenericBean {
     @In
     private UserService userService;
 
+    @Out(scope = ScopeType.SESSION)
+    private User user;
+
     public boolean authenticate() {
         logger.debug(">>> Authentinicating user(username=#{identity.username}), password=#{identity.password}");
-        User user = userService.findUserByUserNameAndPassword(Identity.instance().getUsername(), Identity.instance().getPassword());
+        user = userService.findUserByUserNameAndPassword(Identity.instance().getUsername(), Identity.instance().getPassword());
         logger.debug("<<< Authentinicating ok(result=" + (user != null) + ").");
         return user != null;
     }
