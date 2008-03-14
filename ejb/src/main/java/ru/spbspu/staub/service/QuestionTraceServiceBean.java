a5b02dd1a08645de84fb5c6cdcb963b75898c96f
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-14 16:06:52 +0000

    using TestTrace instance insted of testTraceId

--- ejb/src/main/java/ru/spbspu/staub/service/QuestionTraceServiceBean.java
+++ ejb/src/main/java/ru/spbspu/staub/service/QuestionTraceServiceBean.java
@@ -3,6 +3,7 @@ package ru.spbspu.staub.service;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import ru.spbspu.staub.entity.QuestionTrace;
+import ru.spbspu.staub.entity.TestTrace;
 
 import javax.ejb.Stateless;
 import java.util.List;
@@ -17,8 +18,9 @@ import java.util.List;
 @Stateless
 public class QuestionTraceServiceBean extends GenericServiceBean<QuestionTrace, Integer> implements QuestionTraceService {
     @SuppressWarnings("unchecked")
-    public List<Integer> findIdsByTestTraceId(Integer testTraceId) {
-        return getEntityManager().createQuery("select qt.id from QuestionTrace qt where qt.testTrace.id = :testTraceId").setParameter("testTraceId", testTraceId).getResultList();
+    public List<Integer> findIdsByTestTraceId(TestTrace testTrace) {
+        return getEntityManager().createQuery("select qt.id from QuestionTrace qt where qt.testTrace = :testTrace and qt.finished is null")
+                .setParameter("testTrace", testTrace).getResultList();
     }
 
     public void saveAnswer(QuestionTrace questionTrace) {
