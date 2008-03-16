52fb283ab63da81533b106e50a99b3cb6ecfe9ca
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-16 22:51:52 +0000

    pagination implemented

--- branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/GenericListBean.java
+++ branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/GenericListBean.java
@@ -1,9 +1,12 @@
 package ru.spbspu.staub.bean;
 
-import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.datamodel.DataModel;
 import org.jboss.seam.annotations.datamodel.DataModelSelection;
+import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,21 +14,316 @@ import java.util.List;
  *
  * @author Konstantin Grigoriev
  */
-public abstract class GenericListBean<T> extends GenericBean {
+public abstract class GenericListBean<T> extends GenericModeBean {
 
-    @DataModel
+    @DataModel(scope = ScopeType.PAGE)
     private List<T> dataModel;
 
     @DataModelSelection
     private T selected;
 
-    protected abstract List<T> getResultList();
+    private static enum PageDirection {
+        NEXT_PAGE,
+        PREV_PAGE,
+        LAST_PAGE,
+        FIRST_PAGE,
+        CURRENT_PAGE,
+        EXACT_PAGE
+    }
 
-    @Factory("dataModel")
-    public void fillList() {
-        logger.debug(">>> Constructing list bean...");
-        dataModel = getResultList();
-        logger.debug("<<< Constructing list bean...Ok");
+    /**
+     * Default value for the <code>rowsOnPage</code> instance variable.
+     */
+    private static final int DEFAULT_ROWS_ON_PAGE = 20;
+
+    /**
+     * Total number of pages in selection.
+     */
+    private int pageQuantity = 1;
+
+    /**
+     * How many rows display on every page.
+     */
+    private int rowsOnPage = DEFAULT_ROWS_ON_PAGE;
+
+    /**
+     * Actual number of rows in the current page.
+     * Sometimes it not agrees with <code>rowsOnPage</code>.
+     */
+    private int rowsOnCurrentPage = DEFAULT_ROWS_ON_PAGE;
+
+    /**
+     * Total number of rows in selection.
+     */
+    private int rowsTotal;
+
+    /**
+     * Current page number.
+     */
+    private int currentPage = 1;
+
+    /**
+     * Object representation of the selection data.
+     */
+    protected FormTable formTable;
+
+    /**
+     * Main method for the search (abstract).
+     * <p/>
+     * Every specific subclass must implement this method own way.
+     *
+     * @param formProperties form properties
+     * @return fetched form table
+     */
+    protected abstract FormTable findObjects(FormProperties formProperties);
+
+    public void initBean() {
+        if (isBeanModeDefined()) {
+            logger.debug("Preparing list bean...");
+            switch (getBeanMode()) {
+                case VIEW_MODE:     // using fall through switch behaviour
+                case EDIT_MODE:
+                case CREATE_MODE:
+                    findFirstPageData();
+                    break;
+                case REFRESH_MODE:
+                    doRefresh();
+                    break;
+                default:
+                    logger.debug("  Unknown bean mode -> skipping");
+            }
+            logger.debug("Preparing list bean... OK");
+        }
+    }
+
+    /**
+     * Initializes the page in specific direction.
+     * Defines next page number and retrieves corresponding data.
+     *
+     * @param pageDirection direction for navigation
+     */
+    protected void initPage(PageDirection pageDirection) {
+        logger.debug("Defining page navigation direction...");
+        logger.debug("  Direction is : " + pageDirection);
+        switch (pageDirection) {
+            case NEXT_PAGE:
+                currentPage++;
+                break;
+            case PREV_PAGE:
+                Math.max(1, --currentPage);
+                break;
+            case FIRST_PAGE:
+                currentPage = 1;
+                break;
+            case LAST_PAGE:
+                currentPage = pageQuantity;
+                break;
+            case EXACT_PAGE:
+                logger.debug("    User defined destination page: " + currentPage);
+                break;
+            case CURRENT_PAGE:
+                logger.debug("    Leaving current page number.");
+                break;
+            default:
+                logger.debug("   No action for this direction! Use default " + PageDirection.FIRST_PAGE);
+                currentPage = 1;
+        }
+        logger.debug("  Change current page number to " + currentPage);
+
+        findPageData();
+    }
+
+    /**
+     * Fills properties for the current selection
+     * (current page number, number of pages for retrieve etc).
+     *
+     * @return form properties object
+     */
+    protected FormProperties fillFormProperties() {
+        FormProperties formProperties = new FormProperties();
+        formProperties.setCurrentPage(getCurrentPage());
+        formProperties.setRowsOnPage(getRowsOnPage());
+        return formProperties;
+    }
+
+    /**
+     * Retrieves data for the <code>currentPage</code> page.
+     */
+    private void findPageData() {
+        logger.debug("Defining start row for search...");
+        int startAtRow = 0;
+        if (currentPage >= 1) {
+            startAtRow = (currentPage - 1) * rowsOnPage;
+        }
+        logger.debug("  Start row number is : " + startAtRow);
+
+        formTable = findObjects(fillFormProperties());
+
+        preparePageList();
+    }
+
+    /**
+     * Retrieves data for the first page (initialization).
+     */
+    private void findFirstPageData() {
+        logger.debug("Retrieve first page...");
+        logger.debug("  Define default rows on page...");
+        rowsOnPage = DEFAULT_ROWS_ON_PAGE;
+        rowsOnCurrentPage = DEFAULT_ROWS_ON_PAGE;
+        logger.debug("    Default rows on page : " + rowsOnPage);
+        initPage(PageDirection.FIRST_PAGE);
+        logger.debug("Retrieve completed successfully!");
+    }
+
+    /**
+     * Changes rows on page for the current list.
+     */
+    public void changeRowsOnPage() {
+        throw new UnsupportedOperationException("This operation is not supported");
+    }
+
+    /**
+     * Prepares retrieved data for display.
+     * Defines additional informational characteristics.
+     */
+    private void preparePageList() {
+        logger.debug("Preparing current page for display...");
+        logger.debug("  Defining total number of rows...");
+        rowsTotal = (int) formTable.getFullCount();
+        logger.debug("    Total number of rows is: " + rowsTotal);
+
+        logger.debug("  Defining number of pages..");
+        int quotient = rowsTotal / rowsOnPage;
+        int remainder = rowsTotal % rowsOnPage;
+        if (remainder > 0) {
+            pageQuantity = quotient + 1;
+        } else {
+            pageQuantity = quotient;
+        }
+        logger.debug("    Total page quantity is: " + pageQuantity);
+        logger.debug("    Current page is: " + currentPage);
+
+        if (getCurrentPage() > getPageQuantity() && getPageQuantity() > 0) {
+            logger.debug("      Autoswitch to PREVIOUS page...");
+            initPage(PageDirection.PREV_PAGE);
+            logger.debug("      Autoswitch to PREVIOUS page... OK");
+        }
+
+        logger.debug("  Defining list of objects for display...");
+        if (formTable.getRows() != null) {
+            logger.debug("    Getting objects from form table");
+            dataModel = formTable.getRows();
+        } else {
+            logger.debug("    Creating new objects list");
+            dataModel = new ArrayList<T>();
+        }
+
+        logger.debug("  Defining number of rows on current page...");
+        if (dataModel.size() < rowsOnPage) {
+            rowsOnCurrentPage = dataModel.size();
+        } else {
+            rowsOnCurrentPage = rowsOnPage;
+        }
+        logger.debug("    Number of rows on current page is: " + rowsOnCurrentPage);
+
+    }
+
+    /**
+     * Show/retrieve next page of selection.
+     */
+    public void doNextPage() {
+        logger.debug("Retrieving NEXT page...");
+        initPage(PageDirection.NEXT_PAGE);
+        logger.debug("Retrieving NEXT page... OK");
+    }
+
+    /**
+     * Show/retrieve previous page of selection.
+     */
+    public void doPrevPage() {
+        logger.debug("Retrieving PREVIOUS page...");
+        initPage(PageDirection.PREV_PAGE);
+        logger.debug("Retrieving PREVIOUS page... OK");
+    }
+
+    /**
+     * Show/retrieve first page of selection.
+     */
+    public void doFirstPage() {
+        logger.debug("Retrieving FIRST page...");
+        initPage(PageDirection.FIRST_PAGE);
+        logger.debug("Retrieving FIRST page... OK");
+    }
+
+    /**
+     * Show/retrieve last page of selection.
+     */
+    public void doLastPage() {
+        logger.debug("Retrieving LAST page...");
+        initPage(PageDirection.LAST_PAGE);
+        logger.debug("Retrieving LAST page... OK");
+    }
+
+    /**
+     * Refresh user defined page of selection.
+     */
+    public void doExactPage() {
+        if (getCurrentPage() <= getPageQuantity() && getCurrentPage() >= 1) {
+            logger.debug("Retrieving EXACT page...");
+            initPage(PageDirection.EXACT_PAGE);
+            logger.debug("Retrieving EXACT page... OK");
+        } else {
+            logger.debug("Page number wrong -> staying on the current page");
+        }
+    }
+
+    /**
+     * Refreshes bean content.
+     */
+    public void doRefresh() {
+        logger.debug("Refreshing CURRENT page...");
+        initPage(PageDirection.CURRENT_PAGE);
+        logger.debug("Refreshing CURRENT page... OK");
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPageQuantity() {
+        return pageQuantity;
+    }
+
+    public void setPageQuantity(int pageQuantity) {
+        this.pageQuantity = pageQuantity;
+    }
+
+    public int getRowsOnPage() {
+        return rowsOnPage;
+    }
+
+    public void setRowsOnPage(int rowsOnPage) {
+        this.rowsOnPage = rowsOnPage;
+    }
+
+    public int getRowsOnCurrentPage() {
+        return rowsOnCurrentPage;
+    }
+
+    public void setRowsOnCurrentPage(int rowsOnCurrentPage) {
+        this.rowsOnCurrentPage = rowsOnCurrentPage;
+    }
+
+    public int getRowsTotal() {
+        return rowsTotal;
+    }
+
+    public void setRowsTotal(int rowsTotal) {
+        this.rowsTotal = rowsTotal;
     }
 
     public T getSelected() {
