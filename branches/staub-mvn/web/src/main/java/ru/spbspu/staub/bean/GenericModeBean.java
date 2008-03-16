fe0ea6423409b5435fb643d805c8dc35ace3749e
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-16 18:08:12 +0000

    BeanMode feature introduced

---
+++ branches/staub-mvn/web/src/main/java/ru/spbspu/staub/bean/GenericModeBean.java
@@ -0,0 +1,75 @@
+package ru.spbspu.staub.bean;
+
+import org.jboss.seam.annotations.web.RequestParameter;
+
+/**
+ * Generic webbean implementation, which operates with modes.
+ * Modes can be used for disabling enabling some functionay
+ * and for various bean initializations.
+ *
+ * @author Konstantin Grigoriev
+ * @see BeanMode
+ */
+public abstract class GenericModeBean extends GenericBean {
+    /**
+     * Injected new mode from request.
+     */
+    @RequestParameter("beanMode")
+    private BeanMode beanModeFromRequest;
+    /**
+     * Defines mode of the bean behaviour.
+     */
+    private BeanMode beanMode;
+
+    public void setBeanMode(BeanMode beanMode) {
+        this.beanMode = beanMode;
+    }
+
+    public BeanMode getBeanMode() {
+        return beanMode;
+    }
+
+    /**
+     * Defines bean mode for the current bean.
+     *
+     * @return <code>true</code> if bean mode defined, <code>false</code> - otherwise
+     */
+    public boolean isBeanModeDefined() {
+        if (beanModeFromRequest != null) {
+            logger.debug("Defining bean mode...#0", beanModeFromRequest);
+            setBeanMode(beanModeFromRequest);
+            logger.debug("Defining bean mode... OK");
+            return true;
+        } else {
+            logger.debug("Bean behaviour not specified -> Using old bean state!");
+            return false;
+        }
+    }
+
+    /**
+     * Determines is bean in CREATE_MODE or not.
+     *
+     * @return true of false
+     */
+    public boolean isCreateMode() {
+        return BeanMode.CREATE_MODE.equals(beanMode);
+    }
+
+    /**
+     * Determines is bean in EDIT_MODE or not.
+     *
+     * @return true of false
+     */
+    public boolean isEditMode() {
+        return BeanMode.EDIT_MODE.equals(beanMode);
+    }
+
+    /**
+     * Determines is bean in VIEW_MODE or not.
+     *
+     * @return true of false
+     */
+    public boolean isViewMode() {
+        return BeanMode.VIEW_MODE.equals(beanMode);
+    }
+}
