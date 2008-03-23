4bbfe1cbb631b4c1b698c09dc2dd400fe97f6dd7
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-23 11:43:55 +0000

    findAllTestsToPass method stub added

--- ejb/src/main/java/ru/spbspu/staub/service/TestService.java
+++ ejb/src/main/java/ru/spbspu/staub/service/TestService.java
@@ -1,6 +1,9 @@
 package ru.spbspu.staub.service;
 
 import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.entity.User;
+import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
 
 import javax.ejb.Local;
 
@@ -11,4 +14,12 @@ import javax.ejb.Local;
  */
 @Local
 public interface TestService extends GenericService<Test, Integer> {
+    /**
+     * Fetches all tests assigned for user.
+     *
+     * @param formProperties form properties
+     * @param user specific user
+     * @return result fetch
+     */
+    FormTable findAllToPassForUser(FormProperties formProperties, User user);
 }
