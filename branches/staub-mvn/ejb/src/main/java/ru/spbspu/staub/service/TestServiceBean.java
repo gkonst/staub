34354c9edc9d6db09316dfdad14103a768c37582
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-23 11:43:55 +0000

    findAllTestsToPass method stub added

--- branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestServiceBean.java
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestServiceBean.java
@@ -3,6 +3,9 @@ package ru.spbspu.staub.service;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.entity.User;
+import ru.spbspu.staub.model.list.FormTable;
+import ru.spbspu.staub.model.list.FormProperties;
 
 import javax.ejb.Stateless;
 
@@ -15,4 +18,8 @@ import javax.ejb.Stateless;
 @AutoCreate
 @Stateless
 public class TestServiceBean extends GenericServiceBean<Test, Integer> implements TestService {
+    public FormTable findAllToPassForUser(FormProperties formProperties, User user) {
+        // TODO implement method
+        return findAll(formProperties);
+    }
 }
