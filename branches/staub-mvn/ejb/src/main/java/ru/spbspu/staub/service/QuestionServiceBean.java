262b31fb14aebafa97c4ccf00de20c6c87d4d88b
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-25 15:40:26 +0000

    findAllForTest method stub added

--- branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/QuestionServiceBean.java
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/QuestionServiceBean.java
@@ -3,6 +3,9 @@ package ru.spbspu.staub.service;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import ru.spbspu.staub.entity.Question;
+import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
 
 import javax.ejb.Stateless;
 
@@ -15,4 +18,8 @@ import javax.ejb.Stateless;
 @AutoCreate
 @Stateless
 public class QuestionServiceBean extends GenericServiceBean<Question, Integer> implements QuestionService {
+    public FormTable findAllForTest(FormProperties formProperties, Test test) {
+        // TODO implement method
+        return findAll(formProperties);
+    }
 }
