a8e307b1472ebac3d145f012ad5331723044ff2f
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-25 15:40:26 +0000

    findAllForTest method stub added

--- ejb/src/main/java/ru/spbspu/staub/service/QuestionService.java
+++ ejb/src/main/java/ru/spbspu/staub/service/QuestionService.java
@@ -1,6 +1,9 @@
 package ru.spbspu.staub.service;
 
 import ru.spbspu.staub.entity.Question;
+import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
 
 import javax.ejb.Local;
 
@@ -11,4 +14,12 @@ import javax.ejb.Local;
  */
 @Local
 public interface QuestionService extends GenericService<Question, Integer> {
+    /**
+     * Fetches all questions for specific test.
+     *
+     * @param formProperties form properties
+     * @param test           specific test
+     * @return result fetch
+     */
+    FormTable findAllForTest(FormProperties formProperties, Test test);
 }
