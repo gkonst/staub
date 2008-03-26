6c47b099c48b0d95f3e2c566e205290385ffe0a7
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-26 09:21:46 +0000

    method renamed

--- branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestTraceService.java
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestTraceService.java
@@ -1,7 +1,7 @@
 package ru.spbspu.staub.service;
 
-import ru.spbspu.staub.entity.TestTrace;
 import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.entity.TestTrace;
 import ru.spbspu.staub.entity.User;
 
 import javax.ejb.Local;
@@ -17,12 +17,12 @@ public interface TestTraceService extends GenericService<TestTrace, Integer> {
      * Checks is <code>TestTrace</code> already presents for
      * this user and this sessionId, if not generates it with <code>QuestionTrace</code> entites.
      *
-     * @param test specific <code>Test</code> instance 
+     * @param test specific <code>Test</code> instance
      * @param sessionId <code>HttpSession</code> identifier
      * @param user current user
      * @return created or found <code>TestTrace</code>
      */
-    TestTrace findTestTrace(Test test, String sessionId, User user);
+    TestTrace getTestTrace(Test test, String sessionId, User user);
 
     /**
      * Starts test (fill started field).
