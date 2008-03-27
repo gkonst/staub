9bd6bf88b11ac5f71c0d79fbf6eda5a5b97bc2ba
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-27 16:13:38 +0000

    method stub added

--- branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/QuestionService.java
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/QuestionService.java
@@ -2,6 +2,7 @@ package ru.spbspu.staub.service;
 
 import ru.spbspu.staub.entity.Question;
 import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.entity.User;
 import ru.spbspu.staub.model.list.FormProperties;
 import ru.spbspu.staub.model.list.FormTable;
 
@@ -22,4 +23,14 @@ public interface QuestionService extends GenericService<Question, Integer> {
      * @return result fetch
      */
     FormTable findAllForTest(FormProperties formProperties, Test test);
+
+    /**
+     * Saves question (updates or inserts).
+     * Also updates neede audit fields(created, createdBy, etc.)
+     *
+     * @param question question to save
+     * @param user     current user, author of this modifications
+     * @return updated question
+     */
+    Question saveQuestion(Question question, User user);
 }
