6f518bae192c0ac7cafe9c3e9c8e35451bb45e07
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-27 16:22:02 +0000

    method stub added

--- branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestService.java
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestService.java
@@ -1,11 +1,13 @@
 package ru.spbspu.staub.service;
 
+import ru.spbspu.staub.entity.Question;
 import ru.spbspu.staub.entity.Test;
 import ru.spbspu.staub.entity.User;
 import ru.spbspu.staub.model.list.FormProperties;
 import ru.spbspu.staub.model.list.FormTable;
 
 import javax.ejb.Local;
+import java.util.List;
 
 /**
  * Local Interface for <code>TestServiceBean</code> Stateless EJB.
@@ -18,8 +20,28 @@ public interface TestService extends GenericService<Test, Integer> {
      * Fetches all tests assigned for user.
      *
      * @param formProperties form properties
-     * @param user specific user
+     * @param user           specific user
      * @return result fetch
      */
     FormTable findAllToPassForUser(FormProperties formProperties, User user);
+
+    /**
+     * Adds question for specific test.
+     *
+     * @param test     specific test instance
+     * @param question specific question instance
+     * @param user     specific user instance
+     * @return updates test instance
+     */
+    Test addQuestionToTest(Test test, Question question, User user);
+
+    /**
+     * Adds questions for specific test.
+     *
+     * @param test         specific test instance
+     * @param questionsIds list of question identifiers
+     * @param user         specific user instance
+     * @return updates test instance
+     */
+    Test addQuestionsToTest(Test test, List<Integer> questionsIds, User user);
 }
--- branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestServiceBean.java
+++ branches/staub-mvn/ejb/src/main/java/ru/spbspu/staub/service/TestServiceBean.java
@@ -2,15 +2,17 @@ package ru.spbspu.staub.service;
 
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
+import ru.spbspu.staub.entity.Question;
 import ru.spbspu.staub.entity.Test;
 import ru.spbspu.staub.entity.User;
-import ru.spbspu.staub.model.list.FormTable;
 import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
 
 import javax.ejb.Stateless;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Stateless EJB Service for manipulations with <code>Test</code> entity.
@@ -21,11 +23,24 @@ import java.util.Date;
 @AutoCreate
 @Stateless
 public class TestServiceBean extends GenericServiceBean<Test, Integer> implements TestService {
+    /**
+     * {@inheritDoc}
+     */
     public FormTable findAllToPassForUser(FormProperties formProperties, User user) {
         String query = "select a.test from Assignment a where a.user = :user and a.testBegin <= :currentDate and a.testEnd > :currentDate";
         Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("user", user);
         parameters.put("currentDate", new Date());
         return findAll(query, formProperties, parameters);
+    }
+
+    public Test addQuestionToTest(Test test, Question question, User user) {
+        // TODO implement method
+        return null;
+    }
+
+    public Test addQuestionsToTest(Test test, List<Integer> questionsIds, User user) {
+        // TODO implement method
+        return null;
     }
 }
