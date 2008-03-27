6fb1910f9d843e5d14a45dc2b35a9a67b5762c59
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-03-27 16:13:38 +0000

    method stub added

--- ejb/src/main/java/ru/spbspu/staub/service/QuestionServiceBean.java
+++ ejb/src/main/java/ru/spbspu/staub/service/QuestionServiceBean.java
@@ -4,6 +4,7 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import ru.spbspu.staub.entity.Question;
 import ru.spbspu.staub.entity.Test;
+import ru.spbspu.staub.entity.User;
 import ru.spbspu.staub.model.list.FormProperties;
 import ru.spbspu.staub.model.list.FormTable;
 
@@ -20,10 +21,21 @@ import java.util.Map;
 @AutoCreate
 @Stateless
 public class QuestionServiceBean extends GenericServiceBean<Question, Integer> implements QuestionService {
+    /**
+     * {@inheritDoc}
+     */
     public FormTable findAllForTest(FormProperties formProperties, Test test) {
         String query = "select q from Test t join t.questions q where t = :test";
         Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("test", test);
         return findAll(query, formProperties, parameters);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Question saveQuestion(Question question, User user) {
+        // TODO implement method
+        return question;
     }
 }
