89720e73d5c6144969898cbafb67d5bd055b0304
Alexander.Elagin <Alexander.Elagin@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-04-12 15:05:31 +0000

    Provided methods for an AnswerType construction.

--- ejb/src/main/java/ru/spbspu/staub/model/MultipleChoiceAnswerWrapper.java
+++ ejb/src/main/java/ru/spbspu/staub/model/MultipleChoiceAnswerWrapper.java
@@ -1,22 +1,26 @@
 package ru.spbspu.staub.model;
 
-import ru.spbspu.staub.model.question.AnswerType;
+import ru.spbspu.staub.model.answer.AnswerType;
+import ru.spbspu.staub.model.answer.ElementType;
 import ru.spbspu.staub.model.question.ChoiceType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * TODO add descritpion
+ * The <code>MultipleChoiceAnswerWrapper</code> class is a holder for answer related data. This class is for multiple
+ * choice questions answers.
  *
  * @author Konstantin Grigoriev
+ * @author Alexander V. Elagin
  */
 public class MultipleChoiceAnswerWrapper extends AnswerWrapper {
-    private List<AnswerType> userAnswer;
     private ChoiceType answerDefinition;
 
+    private List<ru.spbspu.staub.model.question.AnswerType> userChoice;
+
     public MultipleChoiceAnswerWrapper(ChoiceType answerDefinition) {
-        userAnswer = new ArrayList<AnswerType>();
+        userChoice = new ArrayList<ru.spbspu.staub.model.question.AnswerType>();
         this.answerDefinition = answerDefinition;
     }
 
@@ -28,11 +32,27 @@ public class MultipleChoiceAnswerWrapper extends AnswerWrapper {
         this.answerDefinition = answerDefinition;
     }
 
-    public List<AnswerType> getUserAnswer() {
-        return userAnswer;
+    public List<ru.spbspu.staub.model.question.AnswerType> getUserChoice() {
+        return userChoice;
     }
 
-    public void setUserAnswer(List<AnswerType> userAnswer) {
-        this.userAnswer = userAnswer;
+    public void setUserChoice(List<ru.spbspu.staub.model.question.AnswerType> userChoice) {
+        this.userChoice = userChoice;
+    }
+
+    public AnswerType getAnswer() {
+        AnswerType answer = null;
+        if (!userChoice.isEmpty()) {
+            answer = new AnswerType();
+            AnswerType.MultipleChoice multipleChoice = new AnswerType.MultipleChoice();
+            List<ElementType> elements = multipleChoice.getElement();
+            for (ru.spbspu.staub.model.question.AnswerType choice : userChoice) {
+                ElementType element = new ElementType();
+                element.setAnswerId(choice.getId());
+                elements.add(element);
+            }
+            answer.setMultipleChoice(multipleChoice);
+        }
+        return answer;
     }
 }
--- ejb/src/main/java/ru/spbspu/staub/model/SingleChoiceAnswerWrapper.java
+++ ejb/src/main/java/ru/spbspu/staub/model/SingleChoiceAnswerWrapper.java
@@ -1,27 +1,31 @@
 package ru.spbspu.staub.model;
 
-import ru.spbspu.staub.model.question.AnswerType;
+import ru.spbspu.staub.model.answer.AnswerType;
+import ru.spbspu.staub.model.answer.ElementType;
 import ru.spbspu.staub.model.question.ChoiceType;
 
 /**
- * TODO add descritpion
+ * The <code>SingleChoiceAnswerWrapper</code> class is a holder for answer related data. This class is for single choice
+ * questions answers.
  *
  * @author Konstantin Grigoriev
+ * @author Alexander V. Elagin
  */
 public class SingleChoiceAnswerWrapper extends AnswerWrapper {
     private ChoiceType answerDefinition;
-    private AnswerType userAnswer;
+
+    private ru.spbspu.staub.model.question.AnswerType userChoice;
 
     public SingleChoiceAnswerWrapper(ChoiceType answerDefinition) {
         this.answerDefinition = answerDefinition;
     }
 
-    public AnswerType getUserAnswer() {
-        return userAnswer;
+    public ru.spbspu.staub.model.question.AnswerType getUserChoice() {
+        return userChoice;
     }
 
-    public void setUserAnswer(AnswerType userAnswer) {
-        this.userAnswer = userAnswer;
+    public void setUserChoice(ru.spbspu.staub.model.question.AnswerType userChoice) {
+        this.userChoice = userChoice;
     }
 
     public ChoiceType getAnswerDefinition() {
@@ -30,5 +34,17 @@ public class SingleChoiceAnswerWrapper extends AnswerWrapper {
 
     public void setAnswerDefinition(ChoiceType answerDefinition) {
         this.answerDefinition = answerDefinition;
+    }
+
+    public ru.spbspu.staub.model.answer.AnswerType getAnswer() {
+        ru.spbspu.staub.model.answer.AnswerType answer = null;
+        if (userChoice != null) {
+            answer = new ru.spbspu.staub.model.answer.AnswerType();
+            AnswerType.SingleChoice singleChoice = new AnswerType.SingleChoice();
+            ElementType element = new ElementType();
+            element.setAnswerId(userChoice.getId());
+            singleChoice.setElement(element);
+        }
+        return answer;
     }
 }
