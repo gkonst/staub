1089b02625cb3aa669ee209f39559e8743f9c3c0
Alexander.Elagin <Alexander.Elagin@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-04-18 04:09:43 +0000

    Refactored / fixed.

--- staub/ejb/src/main/java/ru/spbspu/staub/service/TestService.java
+++ staub/ejb/src/main/java/ru/spbspu/staub/service/TestService.java
@@ -1,54 +1,19 @@
 package ru.spbspu.staub.service;
 
-import ru.spbspu.staub.entity.Question;
 import ru.spbspu.staub.entity.Test;
 import ru.spbspu.staub.entity.User;
-import ru.spbspu.staub.model.list.FormProperties;
-import ru.spbspu.staub.model.list.FormTable;
 
 import javax.ejb.Local;
-import java.util.Date;
 import java.util.List;
 
 /**
  * Local Interface for <code>TestServiceBean</code> Stateless EJB.
  *
  * @author Konstantin Grigoriev
+ * @author Alexander V. Elagin
  */
 @Local
 public interface TestService extends GenericService<Test, Integer> {
-    /**
-     * Fetches all tests assigned for user.
-     *
-     * @param formProperties form properties
-     * @param user           specific user
-     *
-     * @return result fetch
-     */
-    FormTable findAllToPassForUser(FormProperties formProperties, User user);
-
-    /**
-     * Adds question for specific test.
-     *
-     * @param test     specific test instance
-     * @param question specific question instance
-     * @param user     specific user instance
-     *
-     * @return updates test instance
-     */
-    Test addQuestionToTest(Test test, Question question, User user);
-
-    /**
-     * Adds questions for specific test.
-     *
-     * @param test         specific test instance
-     * @param questionsIds list of question identifiers
-     * @param user         specific user instance
-     *
-     * @return updates test instance
-     */
-    Test addQuestionsToTest(Test test, List<Integer> questionsIds, User user);
-
     /**
      * Saves test (updates or inserts).
      * Also updates neede audit fields(created, createdBy, etc.)
@@ -61,12 +26,10 @@ public interface TestService extends GenericService<Test, Integer> {
     Test saveTest(Test test, User user);
 
     /**
-     * Assigns users for specific test.
+     * Assigns students for specific test.
      *
-     * @param testId   specific test identifier
-     * @param usersIds list or users identifiers
-     * @param begin    the lower bound
-     * @param end      the upper bound
+     * @param testId     specific test identifier
+     * @param studentIds list or students identifiers
      */
-    void assignTest(Integer testId, List<Integer> usersIds, Date begin, Date end);
+    void assignTest(Integer testId, List<Integer> studentIds);
 }
