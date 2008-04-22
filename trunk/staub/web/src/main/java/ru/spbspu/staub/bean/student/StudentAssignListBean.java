22cdad89f05ba9af81a93ef78c8058a3a3c48362
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-04-22 10:29:40 +0000

    userAssign renamed to studentAssign, groups added

---
+++ trunk/staub/web/src/main/java/ru/spbspu/staub/bean/student/StudentAssignListBean.java
@@ -0,0 +1,129 @@
+package ru.spbspu.staub.bean.student;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.web.RequestParameter;
+import ru.spbspu.staub.bean.BeanMode;
+import ru.spbspu.staub.bean.GenericListBean;
+import ru.spbspu.staub.entity.Group;
+import ru.spbspu.staub.entity.User;
+import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
+import ru.spbspu.staub.service.GroupService;
+import ru.spbspu.staub.service.StudentService;
+import ru.spbspu.staub.service.TestService;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Webbean for assigning students for specific test.
+ *
+ * @author Konstantin Grigoriev
+ */
+@Name("studentAssignListBean")
+@Scope(SESSION)
+public class StudentAssignListBean extends GenericListBean<User> {
+
+    @RequestParameter("testId")
+    private Integer injectedTestId;
+
+    private Integer testId;
+
+    @In
+    private StudentService studentService;
+
+    @In
+    private GroupService groupService;
+
+    @In
+    private TestService testService;
+
+    private List<Group> groups;
+
+    private Group group;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initBean() {
+        if (isBeanModeDefined()) {
+            logger.debug("Preparing student assign bean...");
+            testId = injectedTestId;
+            // TODO not fully reset bean
+            group = null;
+            setSelectedMap(new HashMap<Object, Boolean>());
+            setDataModel(null);
+            fillGroups();
+            logger.debug("Preparing student assign bean... OK");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected FormTable findObjects(FormProperties formProperties) {
+        return studentService.findStudentsToAssign(formProperties, group);
+    }
+
+    /**
+     * Assignes selected users for current test.
+     */
+    public void assign() {
+        logger.debug(">>> Assigning students...");
+        List<Integer> studentsIds = getSelectedItems();
+        testService.assignTest(testId, studentsIds);
+        logger.debug("  Changing bean mode -> " + BeanMode.VIEW_MODE);
+        setBeanMode(BeanMode.VIEW_MODE);
+        addFacesMessageFromResourceBundle("student.assign.list.assignSuccess");
+        logger.debug("<<< Assigning students...Ok");
+    }
+
+    /**
+     * Defines cancel operation for current bean.
+     *
+     * @return navigation outcome
+     */
+    public String doCancel() {
+        return "testList";
+    }
+
+    /**
+     * Defines back operation for current bean.
+     *
+     * @return navigation outcome
+     */
+    public String doBack() {
+        return "testList";
+    }
+
+    @Override
+    public void doRefresh() {
+        // TODO not fully reset bean
+        if (group != null) {
+            setSelectedMap(new HashMap<Object, Boolean>());
+            super.doRefresh();
+        } else {
+            setDataModel(null);
+        }
+    }
+
+    private void fillGroups() {
+        groups = groupService.findAll();
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+}
