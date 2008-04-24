4228833d93b0e2af7b8fdf495274d4bbcb1ed9b4
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-04-24 15:37:09 +0000

    topics and difficulties modules added, discipline classes moved to single package

---
+++ staub/web/src/main/java/ru/spbspu/staub/bean/difficulty/DifficultyListBean.java
@@ -0,0 +1,40 @@
+package ru.spbspu.staub.bean.difficulty;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import ru.spbspu.staub.bean.GenericListBean;
+import ru.spbspu.staub.entity.Difficulty;
+import ru.spbspu.staub.model.list.FormProperties;
+import ru.spbspu.staub.model.list.FormTable;
+import ru.spbspu.staub.service.DifficultyService;
+
+/**
+ * Webbean for manipulating list data of <code>Difficulty</code> entities.
+ *
+ * @author Konstantin Grigoriev
+ */
+@Name("difficultyListBean")
+@Scope(SESSION)
+public class DifficultyListBean extends GenericListBean<Difficulty> {
+    @In
+    private DifficultyService difficultyService;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected FormTable findObjects(FormProperties formProperties) {
+        return difficultyService.findAll(formProperties);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doDelete() {
+        difficultyService.remove(getSelected());
+        doRefresh();
+    }
+}
---
+++ staub/web/src/main/java/ru/spbspu/staub/bean/topic/TopicDetailBean.java
@@ -0,0 +1,94 @@
+package ru.spbspu.staub.bean.topic;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import ru.spbspu.staub.bean.BeanMode;
+import ru.spbspu.staub.bean.GenericDetailBean;
+import ru.spbspu.staub.entity.Category;
+import ru.spbspu.staub.entity.Discipline;
+import ru.spbspu.staub.entity.Topic;
+import ru.spbspu.staub.service.CategoryService;
+import ru.spbspu.staub.service.DisciplineService;
+import ru.spbspu.staub.service.TopicService;
+
+import java.util.List;
+
+/**
+ * Webbean for manipulating detail data of <code>Topic</code> entity.
+ *
+ * @author Konstantin Grigoriev
+ */
+@Name("topicDetailBean")
+@Scope(SESSION)
+public class TopicDetailBean extends GenericDetailBean<Topic> {
+
+    @In
+    private DisciplineService disciplineService;
+
+    @In
+    private CategoryService categoryService;
+
+    @In
+    private TopicService topicService;
+
+    private List<Discipline> disciplineList;
+    private List<Category> categoryList;
+
+    private Discipline discipline;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void fillModel(Integer modelId) {
+        fillDisciplineList();
+        if (isCreateMode()) {
+            setModel(new Topic());
+        } else {
+            setModel(topicService.findById(modelId));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doSave() {
+        logger.debug("Saving topic...");
+        setModel(topicService.makePersistent(getModel()));
+        logger.debug("  Changing bean mode -> " + BeanMode.VIEW_MODE);
+        setBeanMode(BeanMode.VIEW_MODE);
+        addFacesMessageFromResourceBundle("common.messages.saveSuccess");
+        logger.debug("Saving... OK");
+    }
+
+    private void fillDisciplineList() {
+        disciplineList = disciplineService.findAll();
+    }
+
+    public void refreshCategories() {
+        if (discipline != null) {
+            categoryList = categoryService.getCategories(discipline);
+        } else {
+            categoryList = null;
+        }
+    }
+
+    public List<Discipline> getDisciplineList() {
+        return disciplineList;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public Discipline getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(Discipline discipline) {
+        this.discipline = discipline;
+    }
+}
