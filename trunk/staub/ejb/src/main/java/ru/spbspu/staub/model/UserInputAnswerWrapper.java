36f2c793999632a06b532ea78b4f1754f35a0757
Alexander.Elagin <Alexander.Elagin@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-04-29 09:56:12 +0000

    Unnecessary check removed.

--- trunk/staub/ejb/src/main/java/ru/spbspu/staub/model/UserInputAnswerWrapper.java
+++ trunk/staub/ejb/src/main/java/ru/spbspu/staub/model/UserInputAnswerWrapper.java
@@ -55,63 +55,57 @@ public class UserInputAnswerWrapper extends AnswerWrapper<UserInputType, String>
             LOG.debug("*  Regexp for integer.");
             Pattern pattern = Pattern.compile(REGEX_MAP.get(InputType.INTEGER));
             Matcher matcher = pattern.matcher(answer);
-            if (matcher.matches()) { // TODO: Throw an exception if false?
-                regexp.append("\\s*");
-                String minus = matcher.group(1);
-                if ((minus != null) && (minus.length() > 0)) {
-                    regexp.append("-\\s*");
-                }
-                regexp.append(matcher.group(2));
-                regexp.append("\\s*");
+            regexp.append("\\s*");
+            String minus = matcher.group(1);
+            if ((minus != null) && (minus.length() > 0)) {
+                regexp.append("-\\s*");
             }
+            regexp.append(matcher.group(2));
+            regexp.append("\\s*");
         } else if (isReal()) {
             LOG.debug("*  Regexp for real.");
             Pattern pattern = Pattern.compile(REGEX_MAP.get(InputType.REAL));
             Matcher matcher = pattern.matcher(answer);
-            if (matcher.matches()) {
-                regexp.append("\\s*");
-                String minus = matcher.group(1);
-                if ((minus != null) && (minus.length() > 0)) {
-                    regexp.append("-\\s*");
-                }
-                regexp.append(matcher.group(2));
-                String fraction = matcher.group(3);
-                if ((fraction != null) && (fraction.length() > 0)) {
-                    regexp.append("[\\.,]").append(fraction);
-                }
-                regexp.append("\\s*");
+            regexp.append("\\s*");
+            String minus = matcher.group(1);
+            if ((minus != null) && (minus.length() > 0)) {
+                regexp.append("-\\s*");
             }
+            regexp.append(matcher.group(2));
+            String fraction = matcher.group(3);
+            if ((fraction != null) && (fraction.length() > 0)) {
+                regexp.append("[\\.,]").append(fraction);
+            }
+            regexp.append("\\s*");
         } else if (isComplex()) {
             LOG.debug("*  Regexp for complex.");
             Pattern pattern = Pattern.compile(REGEX_MAP.get(InputType.COMPLEX));
             Matcher matcher = pattern.matcher(answer);
-            if (matcher.matches()) {
+            regexp.append("\\s*");
+            String minus = matcher.group(1);
+            if ((minus != null) && (minus.length() > 0)) {
+                regexp.append("-\\s*");
+            }
+            regexp.append(matcher.group(2));
+            String fraction = matcher.group(3);
+            if ((fraction != null) && (fraction.length() > 0)) {
+                regexp.append("[\\.,]").append(fraction);
+            }
+            regexp.append("\\s*");
+            String sign = matcher.group(4);
+            if ((sign != null) && (sign.length() > 0)) {
+                if ("+".equals(sign)) {
+                    regexp.append("\\+");
+                } else {
+                    regexp.append("-");
+                }
                 regexp.append("\\s*");
-                String minus = matcher.group(1);
-                if ((minus != null) && (minus.length() > 0)) {
-                    regexp.append("-\\s*");
+                regexp.append(matcher.group(5));
+                String imaginaryFraction = matcher.group(6);
+                if ((imaginaryFraction != null) && (imaginaryFraction.length() > 0)) {
+                    regexp.append("[\\.,]").append(imaginaryFraction);
                 }
-                regexp.append(matcher.group(2));
-                String fraction = matcher.group(3);
-                if ((fraction != null) && (fraction.length() > 0)) {
-                    regexp.append("[\\.,]").append(fraction);
-                }
-                regexp.append("\\s*");
-                String sign = matcher.group(4);
-                if ((sign != null) && (sign.length() > 0)) {
-                    if ("+".equals(sign)) {
-                        regexp.append("\\+");
-                    } else {
-                        regexp.append("-");
-                    }
-                    regexp.append("\\s*");
-                    regexp.append(matcher.group(5));
-                    String imaginaryFraction = matcher.group(6);
-                    if ((imaginaryFraction != null) && (imaginaryFraction.length() > 0)) {
-                        regexp.append("[\\.,]").append(imaginaryFraction);
-                    }
-                    regexp.append("\\s*\\*?\\s*[ij]\\s*");
-                }
+                regexp.append("\\s*\\*?\\s*[ij]\\s*");
             }
         } else if (isString()) {
             LOG.debug("*  Answer is a string. Regexp construction skipped.");
