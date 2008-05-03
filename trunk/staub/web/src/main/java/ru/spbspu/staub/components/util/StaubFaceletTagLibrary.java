67051d8f188327104fe17dd08a8c3e3524d4003f
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-05-03 19:14:33 +0000

    asList jstl function added

--- trunk/staub/web/src/main/java/ru/spbspu/staub/components/util/StaubFaceletTagLibrary.java
+++ trunk/staub/web/src/main/java/ru/spbspu/staub/components/util/StaubFaceletTagLibrary.java
@@ -1,7 +1,12 @@
 package ru.spbspu.staub.components.util;
 
+import com.sun.facelets.FaceletException;
 import com.sun.facelets.tag.AbstractTagLibrary;
+import ru.spbspu.staub.components.function.Functions;
 import ru.spbspu.staub.components.validator.RegexValidator;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
 
 /**
  * Facelet library definition for tags in Staub Application.
@@ -11,9 +16,18 @@ import ru.spbspu.staub.components.validator.RegexValidator;
 public final class StaubFaceletTagLibrary extends AbstractTagLibrary {
 
     public final static String NAMESPACE = "http://spbspu.ru/staub/taglib";
-    
+
     public StaubFaceletTagLibrary() {
         super(NAMESPACE);
         this.addValidator("validateRegex", RegexValidator.VALIDATOR_ID);
+        this.addFunction("asList", getFunction("asList", Collection.class));
+    }
+
+    private Method getFunction(String name, Class... parameterTypes) {
+        try {
+            return Functions.class.getMethod(name, parameterTypes);
+        } catch (NoSuchMethodException e) {
+            throw new FaceletException(e);
+        }
     }
 }
