b12986e04d38a14e9f70eac5aa9c76b3bb57d10d
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-05-03 16:18:48 +0000

    student module implemented

---
+++ staub/web/src/main/java/ru/spbspu/staub/bean/student/StudentDetailBean.java
@@ -0,0 +1,72 @@
+package ru.spbspu.staub.bean.student;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.contexts.Contexts;
+import ru.spbspu.staub.bean.BeanMode;
+import ru.spbspu.staub.bean.GenericDetailBean;
+import ru.spbspu.staub.entity.Group;
+import ru.spbspu.staub.entity.Student;
+import ru.spbspu.staub.service.GroupService;
+import ru.spbspu.staub.service.StudentService;
+
+import java.util.List;
+
+/**
+ * Webbean for manipulating detail data of <code>Student</code> entity.
+ *
+ * @author Konstantin Grigoriev
+ */
+@Name("studentDetailBean")
+@Scope(SESSION)
+public class StudentDetailBean extends GenericDetailBean<Student> {
+    private static final long serialVersionUID = 7860014639037606020L;
+
+    @In
+    private StudentService studentService;
+
+    @In
+    private GroupService groupService;
+
+    private List<Group> groupList;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void fillModel(Integer modelId) {
+        fillGroupList();
+        if (isCreateMode()) {
+            setModel(new Student());
+            Group group = (Group) Contexts.getConversationContext().get(Group.class.getName());
+            if (group != null) {
+                getModel().setGroup(group);
+            }
+        } else {
+            setModel(studentService.findById(modelId));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doSave() {
+        logger.debug("Saving student...");
+        setModel(studentService.saveStudent(getModel()));
+        logger.debug("  Changing bean mode -> " + BeanMode.VIEW_MODE);
+        setBeanMode(BeanMode.VIEW_MODE);
+        addFacesMessageFromResourceBundle("common.messages.saveSuccess");
+        logger.debug("Saving... OK");
+    }
+
+    private void fillGroupList() {
+        groupList = groupService.findAll();
+    }
+
+    public List<Group> getGroupList() {
+        return groupList;
+    }
+}
