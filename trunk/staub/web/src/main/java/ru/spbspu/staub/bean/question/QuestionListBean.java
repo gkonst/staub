0b62adda927ec9349d33b7f54f459e1087807baa
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-05-03 11:49:07 +0000

    filter for questions added

--- trunk/staub/web/src/main/java/ru/spbspu/staub/bean/question/QuestionListBean.java
+++ trunk/staub/web/src/main/java/ru/spbspu/staub/bean/question/QuestionListBean.java
@@ -4,12 +4,15 @@ import static org.jboss.seam.ScopeType.SESSION;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.contexts.Contexts;
 import ru.spbspu.staub.bean.GenericListBean;
-import ru.spbspu.staub.entity.Question;
+import ru.spbspu.staub.entity.*;
 import ru.spbspu.staub.exception.RemoveException;
 import ru.spbspu.staub.model.list.FormProperties;
 import ru.spbspu.staub.model.list.FormTable;
-import ru.spbspu.staub.service.QuestionService;
+import ru.spbspu.staub.service.*;
+
+import java.util.List;
 
 /**
  * Webbean for manipulating list data of <code>Question</code> entities.
@@ -24,12 +27,42 @@ public class QuestionListBean extends GenericListBean<Question> {
     @In
     private QuestionService questionService;
 
+    @In
+    private DisciplineService disciplineService;
+
+    @In
+    private CategoryService categoryService;
+
+    @In
+    private TopicService topicService;
+
+    @In
+    private DifficultyService difficultyService;
+
+    private List<Discipline> disciplineList;
+    private List<Category> categoryList;
+    private List<Topic> topicList;
+    private List<Difficulty> difficultyList;
+    private Discipline discipline;
+    private Category category;
+    private Topic topic;
+    private Difficulty difficulty;
+
     /**
      * {@inheritDoc}
      */
     @Override
     protected FormTable findObjects(FormProperties formProperties) {
-        return questionService.findAll(formProperties);
+        return questionService.findQuestions(formProperties, discipline, category, topic, difficulty);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void prepareBean() {
+        fillDisciplineList();
+        fillDifficultiesList();
     }
 
     /**
@@ -45,4 +78,109 @@ public class QuestionListBean extends GenericListBean<Question> {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String doCreate() {
+        Contexts.getConversationContext().set(Discipline.class.getName(), discipline);
+        Contexts.getConversationContext().set(Category.class.getName(), category);
+        Contexts.getConversationContext().set(Topic.class.getName(), topic);
+        Contexts.getConversationContext().set(Difficulty.class.getName(), difficulty);
+        return super.doCreate();
+    }
+
+    private void fillDisciplineList() {
+        disciplineList = disciplineService.findAll();
+    }
+
+    private void refreshCategories() {
+        if (discipline != null) {
+            categoryList = categoryService.findCategories(discipline);
+        } else {
+            categoryList = null;
+        }
+    }
+
+    private void refreshTopics() {
+        if (category != null) {
+            topicList = topicService.findTopics(category);
+        } else {
+            topicList = null;
+        }
+    }
+
+    private void fillDifficultiesList() {
+        difficultyList = difficultyService.findAll();
+    }
+
+    public void setDiscipline() {
+        refreshCategories();
+        setCategory(null);
+        setTopic(null);
+        doRefresh();
+    }
+
+    public void setCategory() {
+        refreshTopics();
+        setTopic(null);
+        doRefresh();
+    }
+
+    public void setTopic() {
+        doRefresh();
+    }
+
+    public void setDifficulty() {
+        doRefresh();
+    }
+
+    public List<Discipline> getDisciplineList() {
+        return disciplineList;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public List<Topic> getTopicList() {
+        return topicList;
+    }
+
+    public List<Difficulty> getDifficultyList() {
+        return difficultyList;
+    }
+
+    public Discipline getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(Discipline discipline) {
+        this.discipline = discipline;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+
+    }
+
+    public Topic getTopic() {
+        return topic;
+    }
+
+    public void setTopic(Topic topic) {
+        this.topic = topic;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
