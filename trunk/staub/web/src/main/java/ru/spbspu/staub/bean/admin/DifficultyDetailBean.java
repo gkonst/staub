c60f49fd99289b6504c06d4725072fb19ecfcf95
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-05-22 14:31:54 +0000

    package refactoring

---
+++ trunk/staub/web/src/main/java/ru/spbspu/staub/bean/admin/DifficultyDetailBean.java
@@ -0,0 +1,61 @@
+package ru.spbspu.staub.bean.admin;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import ru.spbspu.staub.bean.BeanMode;
+import ru.spbspu.staub.bean.GenericDetailBean;
+import ru.spbspu.staub.entity.Difficulty;
+import ru.spbspu.staub.service.DifficultyService;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * Webbean for manipulating detail data of <code>Difficulty</code> entity.
+ *
+ * @author Konstantin Grigoriev
+ */
+@Name("difficultyDetailBean")
+@Scope(SESSION)
+public class DifficultyDetailBean extends GenericDetailBean<Difficulty> {
+    private static final long serialVersionUID = 4972878435691024078L;
+
+    @In
+    private DifficultyService difficultyService;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void fillModel(Integer modelId) {
+        if (isCreateMode()) {
+            setModel(new Difficulty());
+        } else {
+            setModel(difficultyService.findById(modelId));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doSave() {
+        logger.debug("Saving difficulty...");
+        setModel(difficultyService.makePersistent(getModel()));
+        logger.debug("  Changing bean mode -> " + BeanMode.VIEW_MODE);
+        setBeanMode(BeanMode.VIEW_MODE);
+        addFacesMessageFromResourceBundle("common.messages.saveSuccess");
+        logger.debug("Saving... OK");
+    }
+
+    public void validateCode(FacesContext facesContext, UIComponent uiComponent, Object value) {
+        if (!difficultyService.isCodeUnique((Integer) value)) {
+            throw new ValidatorException(new FacesMessage("Difficulty code must be unique"));
+        }
+    }
+}
+
---
+++ trunk/staub/web/src/main/java/ru/spbspu/staub/bean/admin/GroupDetailBean.java
@@ -0,0 +1,60 @@
+package ru.spbspu.staub.bean.admin;
+
+import static org.jboss.seam.ScopeType.SESSION;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import ru.spbspu.staub.bean.BeanMode;
+import ru.spbspu.staub.bean.GenericDetailBean;
+import ru.spbspu.staub.entity.Group;
+import ru.spbspu.staub.service.GroupService;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * Webbean for manipulating detail data of <code>Group</code> entity.
+ *
+ * @author Konstantin Grigoriev
+ */
+@Name("groupDetailBean")
+@Scope(SESSION)
+public class GroupDetailBean extends GenericDetailBean<Group> {
+    private static final long serialVersionUID = -8391481196457853709L;
+
+    @In
+    private GroupService groupService;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void fillModel(Integer modelId) {
+        if (isCreateMode()) {
+            setModel(new Group());
+        } else {
+            setModel(groupService.findById(modelId));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doSave() {
+        logger.debug("Saving group...");
+        setModel(groupService.makePersistent(getModel()));
+        logger.debug("  Changing bean mode -> " + BeanMode.VIEW_MODE);
+        setBeanMode(BeanMode.VIEW_MODE);
+        addFacesMessageFromResourceBundle("common.messages.saveSuccess");
+        logger.debug("Saving... OK");
+    }
+
+    public void validateName(FacesContext facesContext, UIComponent uiComponent, Object value) {
+        if (!groupService.isNameUnique(String.valueOf(value))) {
+            throw new ValidatorException(new FacesMessage("Group name must be unique"));
+        }
+    }
+}
