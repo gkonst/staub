e5437f90d3274b30371af21a65fa4663ff6c6bb9
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-06-03 13:02:25 +0000

    test data and initial data splitted

--- staub/db/src/main/resources/test_data.sql
+++ staub/db/src/main/resources/test_data.sql
@@ -1,15 +1,3 @@
-INSERT INTO role (id, description) VALUES (0, 'Administrator');
-INSERT INTO role (id, description) VALUES (1, 'Test Writer');
-
-INSERT INTO "user" (id,
-                    fk_role,
-                    username,
-                    password)
-     VALUES (nextval('seq_user'),
-             0,
-             'admin',
-             'd033e22ae348aeb5660fc2140aec35850c4da997');
-
 INSERT INTO "user" (id,
                     fk_role,
                     username,
