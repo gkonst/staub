0bc80cafdacddb3595b3a4a3d3952679451d769d
Konstantin.V.Grigoriev <Konstantin.V.Grigoriev@72f5af75-133d-0410-a2a4-d11a6d6508d2>
2008-06-15 14:57:25 +0000

    Null values fixed

--- staub/web/src/main/java/ru/spbspu/staub/components/converter/TimeConverter.java
+++ staub/web/src/main/java/ru/spbspu/staub/components/converter/TimeConverter.java
@@ -21,50 +21,58 @@ public class TimeConverter implements Converter {
      * {@inheritDoc}
      */
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        if (!value.matches(TIME_PATTERN)) {
-            throw new ConverterException(MessageFactory.getMessage(
-                    context, "ru.spbspu.staub.TimeConverter", value,
-                    "01:10:00",
-                    MessageFactory.getLabel(context, component)));
-        }
-        Integer result = 0;
-        StringTokenizer st = new StringTokenizer(value, ":");
-        String token;
-        if (st.countTokens() == 3) {
+        if (value != null && value.length() != 0) {
+            if (!value.matches(TIME_PATTERN)) {
+                throw new ConverterException(MessageFactory.getMessage(
+                        context, "ru.spbspu.staub.TimeConverter", value,
+                        "01:10:00",
+                        MessageFactory.getLabel(context, component)));
+            }
+            Integer result = 0;
+            StringTokenizer st = new StringTokenizer(value, ":");
+            String token;
+            if (st.countTokens() == 3) {
+                token = st.nextToken();
+                result = 3600 * Integer.valueOf(token);
+            }
             token = st.nextToken();
-            result = 3600 * Integer.valueOf(token);
+            result = result + 60 * Integer.valueOf(token);
+            token = st.nextToken();
+            result = result + Integer.valueOf(token);
+            return result;
+        } else {
+            return null;
         }
-        token = st.nextToken();
-        result = result + 60 * Integer.valueOf(token);
-        token = st.nextToken();
-        result = result + Integer.valueOf(token);
-        return result;
     }
 
     /**
      * {@inheritDoc}
      */
     public String getAsString(FacesContext context, UIComponent component, Object value) {
-        int seconds = (Integer) value;
-        int minutes = 0;
-        int hours = 0;
-        if (seconds >= 60) {
-            minutes = seconds / 60;
-            seconds = seconds % 60;
-            if (minutes >= 60) {
-                hours = minutes / 60;
-                minutes = minutes % 60;
+        if (value != null) {
+            int seconds = (Integer) value;
+            int minutes = 0;
+            int hours = 0;
+            if (seconds >= 60) {
+                minutes = seconds / 60;
+                seconds = seconds % 60;
+                if (minutes >= 60) {
+                    hours = minutes / 60;
+                    minutes = minutes % 60;
+                }
             }
-        }
-        StringBuilder result = new StringBuilder();
-        if (hours != 0) {
-            result.append(lpad(String.valueOf(hours), 2, "0"));
+            StringBuilder result = new StringBuilder();
+            if (hours != 0) {
+                result.append(lpad(String.valueOf(hours), 2, "0"));
+                result.append(":");
+            }
+            result.append(lpad(String.valueOf(minutes), 2, "0"));
             result.append(":");
+            result.append(lpad(String.valueOf(seconds), 2, "0"));
+            return result.toString();
+        } else {
+            return null;
         }
-        result.append(lpad(String.valueOf(minutes), 2, "0"));
-        result.append(":");
-        result.append(lpad(String.valueOf(seconds), 2, "0"));
-        return result.toString();
     }
 
     private String lpad(String string, int n, String symbol) {
